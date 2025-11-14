87df335b05e14a87517093aa9787625a91584068
Janina Rantala <janina.rantala@centria.fi>
2025-11-14 14:42:48 +0200

    sisältö

--- Viikko2/lue_varaukset.py
+++ Viikko2/lue_varaukset.py
@@ -5,6 +5,8 @@ def main():
     # Avataan tiedosto ja luetaan sisältö
     with open(varaukset, "r", encoding="utf-8") as f:
         varaus = f.read().strip()
+
         def main():
         print(varaus)
+        
     if __name__ == "__main__":
