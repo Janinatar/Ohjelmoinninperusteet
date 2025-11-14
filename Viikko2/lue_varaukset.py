051ed9717b2603af128bb459af2ff7183819f86f
Janina Rantala <janina.rantala@centria.fi>
2025-11-14 15:12:46 +0200

    testiä

--- Viikko2/lue_varaukset.py
+++ Viikko2/lue_varaukset.py
@@ -1,11 +1,38 @@
-def main():
-    varaukset = "varaukset.txt"
+def lue_varaus():
+    # Luetaan tiedosto
+    with open("varaukset.txt", "r", encoding="utf-8") as f:
+        rivi = f.readline().strip()
 
-    with open(varaukset, "r", encoding="utf-8") as f:
-        varaus = f.read().strip()
+    
+    osat = rivi.split("|")
 
-        def main():
-        print(varaukset.split('|'))
+    varausnumero = osat[0]
+    varaaja = osat[1]
+    pvm = osat[2]
+    aloitusaika = osat[3]
+    tuntimaara = float(osat[4])
+    tuntihinta = float(osat[5])
+    maksettu = osat[6]
+    kohde = osat[7]
+    puhelin = osat[8]
+    sahkoposti = osat[9]
 
-    if __name__ == "__main__":
-        main()
+    # Laske kokonaishinta
+    kokonaishinta = tuntimaara * tuntihinta
+
+    # Tulostus täsmälleen vaaditussa muodossa
+    print(f"Varausnumero: {varausnumero}")
+    print(f"Varaaja: {varaaja}")
+    print(f"Päivämäärä: {pvm}")
+    print(f"Aloitusaika: {aloitusaika}")
+    print(f"Tuntimäärä: {tuntimaara:g}")
+    print(f"Tuntihinta: {tuntihinta} €")
+    print(f"Kokonaishinta: {kokonaishinta} €")
+    print(f"Maksettu: {maksettu}")
+    print(f"Kohde: {kohde}")
+    print(f"Puhelin: {puhelin}")
+    print(f"Sähköposti: {sahkoposti}")
+
+
+if __name__ == "__main__":
+    lue_varaus()
