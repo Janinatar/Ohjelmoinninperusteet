8f1f9d487d3c65e4603e51b88065c5a23eba182d
Janina Rantala <janina.rantala@centria.fi>
2025-11-14 11:52:29 +0200

    uusi kansia viikk2

---
+++ Viikko2/lue_varaukset.py
@@ -0,0 +1,10 @@
+def main():
+    # Määritellään tiedoston nimi suoraan koodissa
+    varaukset = "varaukset.txt"
+
+    # Avataan tiedosto ja luetaan sisältö
+    with open(varaukset, "r", encoding="utf-8") as f:
+        varaus = f.read().strip()
+        def main():
+        print(varaus)
+    if __name__ == "__main__":
